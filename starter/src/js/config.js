693dda33a2299718256587a9b8d72c1d5ec5b661
Rhythm Gupta <104640619+rhhythmmm@users.noreply.github.com>
2022-12-23 16:58:02 +0530

    Delete starter directory

--- starter/src/js/config.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
-export const TIMEOUT = 10;
-export const RES_PER_PAGE = 10;
-export const KEY = 'd4fc0e4d-1732-46ea-8504-979d78a26cac';
-export const MODAL_CLOSE_SEC =2.5;
